4e468716bba62a90ceebb3d2f32e8c0695690935
siwakorn-chantawat <siwakorn.chantawat@gmail.com>
2023-12-24 22:41:03 +0700

    finish tailwind course

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,18 +4,19 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: "#DEF81D",
+        primary: "#FFA732",
         "wd-white": "#FFFFFF",
+        "wd-brand": "#898989",
         "wd-grey": "#999999",
         "wd-darkgrey": "#2D2D2D",
         "wd-black": "#080808",
       },
       backgroundImage: {
         "hero-pattern":
-          "linear-gradient(to left, rgba(0,0,0, 0), rgba(0,0,0,1)), url('../assets/hero/hero.jpg')",
+          "linear-gradient(to left, rgba(0,0,0, 0.25), rgba(0,0,0,1)), url('../assets/hero/hero.jpg')",
       },
       fontFamily: {
-        sans: ["poppins", "Arial", "sans-serif"],
+        sans: ["poppin", "Arial", "sans-serif"],
       }
     },
   },
